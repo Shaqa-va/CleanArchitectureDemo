539557a82a28f342d871c6728262a3571e32ad9c
Shaqa-va <shaqavaziri1@gmail.com>
2020-12-21 23:52:01 +0330

    Commit Course Service and Repository and ViewModel

---
+++ CleanArchitectureDemo/CleanArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Interfaces
+{
+  public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+      
+
+    }
+}
